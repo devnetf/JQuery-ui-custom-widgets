45016131f6504e347dab41a1c10430cc3df704e4
Shao Hang He <shaohanghe91@gmail.com>
2013-10-23 18:35:44 -0400

    Update hmenu.js

--- hmenu/hmenu.js
+++ hmenu/hmenu.js
@@ -11,9 +11,12 @@
         width : '',
         height : '',
         backgroundColor: '#C0FFFF',
+        fontColor: '#1E1E1E',
         borderBottom : '4px solid #969696',
-        borderRadius : ' 5px 5px 0px 0px',
+        borderRadius : '5px 5px 0px 0px',
         margin: '0px 0px 0px 0px',
+        borderRadiusMainItems : '0px',
+        itemsMargin : '0px 0px 0px 4px',
         itemsFontWeight : 'bold',
         itemsFontSize : '12px',
         itemsPaddingTop: '5px',
@@ -21,52 +24,54 @@
         itemsPaddingLeft: '5px',
         itemsPaddingRight: '5px',
         activeItemColor: '#969696',
-        dropdownItemColor: '#616161',
+        hoverFontColor: '#fff',
+        dropdownItemWidth: '',
+        dropdownItemHeight: '',
+        dropdownItemColor: '#C0FFFF',
+        dropdownHoverColor: '#969696',
         dropdownItemOpacity: 1.0,
         data: null,
       },
 
-      global_vars: {
-        OnClickCallbacks : new Object(),
-        ItemIds : [],
-        ItemCounter : 0
-      },
       // the constructor
       _create: function() {
         
         var curr = this;
-
+        this.element.data('ItemIds', []);
+        this.element.data('OnClickCallbacks', {});
+        this.element.data('ItemCounter', 0);
+        
         if(this.options.data !== null)
         {
             this.element.html(this.createList(this.options.data));
         }
 
-        for(var i = 0; i < this.global_vars.ItemIds.length; i++)
+        for(var i = 0; i < this.element.data('ItemIds').length; i++)
         {
-            $('#' + this.global_vars.ItemIds[i]).bind( 'click', this.global_vars.OnClickCallbacks[this.global_vars.ItemIds[i]]);
+            $('#' + this.element.data('ItemIds')[i]).on( 'click',  this.element.data('OnClickCallbacks')[this.element.data('ItemIds')[i]]);
         }
 
         this.element.addClass( "ui-hmenu" ).disableSelection();
 
         curr.element.children('ul').children('li').children('a').click(function (){
-
+            
             curr.element.find('ul li.ui-active').each(function (){
 
                 $(this).removeClass('ui-active');
                 $(this).children('a').attr("style",  '');
                 $(this).css("background",  curr.options.backgroundColor);
-
+                $(this).css("color",  curr.options.fontColor); 
             });
 
             $(this).parent().addClass('ui-active');
             $(this).children('a').attr("style",  '');
             $(this).parent().css("background",  curr.options.activeItemColor);
+            $(this).css("color",  curr.options.hoverFontColor);
 
             curr._refresh();
         });
 
         this._refresh();
-
       },
  
       // called when created, and later when changing options
@@ -86,16 +91,41 @@
         curr.element.css( "padding-left", this.options.itemsPaddingLeft);
         curr.element.css( "padding-right", this.options.itemsPaddingRight);
 
-        
-        curr.element.find('ul li.ui-active > a').css("background",  this.options.activeItemColor).unbind('mouseenter mouseleave');
-        curr.element.find('ul li:not(.ui-active)').children('a').hover(
-          function (){ $(this).css("background",  curr.options.activeItemColor); }, 
-          function (){ $(this).css("background",  curr.options.backgroundColor); }
+        curr.element.children('ul').children('li:not(.ui-active)').children('a').hover(
+          function ()
+          { 
+            $(this).css("background",  curr.options.activeItemColor);
+            $(this).css("color",  curr.options.hoverFontColor); 
+          }, 
+          function ()
+          { 
+            //default color
+            $(this).css("background",  curr.options.backgroundColor); 
+            $(this).css("color",  curr.options.fontColor); 
+          }
         );
 
-        curr.element.find('ul ul a').css("background", this.options.dropdownItemColor);
-        curr.element.find('ul ul a').css("opacity", this.options.dropdownItemOpacity);
-        curr.element.find('ul ul').css("border-top-color",  this.options.activeItemColor);
+        curr.element.children('ul').children('li.ui-active').children('a').css("background",  this.options.activeItemColor)
+                                                                          .off('mouseenter mouseleave');
+        
+        curr.element.find('ul ul li').hover(
+          function (){ $(this).children('a').css("background",  curr.options.dropdownHoverColor); }, 
+          function (){ $(this).children('a').css("background",  curr.options.dropdownItemColor); }
+        );
+
+        curr.element.find('.ui-active').css("background",  this.options.activeItemColor)
+                                       .css("color",  this.options.hoverFontColor);
+
+        curr.element.find('ul ul a').css("background", this.options.dropdownItemColor)
+                                    .css("opacity", this.options.dropdownItemOpacity);
+
+        curr.element.find('ul ul').css("border-top-color",  this.options.activeItemColor)
+                                  .css("width",  this.options.dropdownItemWidth)
+                                  .css("height",  this.options.dropdownItemHeight);
+
+        curr.element.children('ul').children('li').css('border-radius', this.options.borderRadiusMainItems)
+                                                  .css('margin', this.options.itemsMargin)
+                                                  .children('a').css('border-radius', this.options.borderRadiusMainItems);
 
         // trigger a callback/event
         this._trigger( "change" );
@@ -108,9 +138,9 @@
         this.element.attr('style', '');
         this.element.removeClass( "ui-hmenu" ).enableSelection();
 
-        for(var i = 0; i < this.global_vars.ItemIds.length; i++)
+        for(var i = 0; i < this.element.data('ItemIds').length; i++)
         {
-            $('#' + this.global_vars.ItemIds[i]).unbind( 'click' );
+          $('#' + this.element.data('ItemIds')[i]).off( 'click' );
         }
 
       },
@@ -148,8 +178,10 @@
             var hasSub  = "";
             var active = "";
             var subContent = "";
-            var itemId = 'MeneuItem_' + this.makeId(10) + this.global_vars.ItemCounter++;
+            var itemId = 'MeneuItem_' + this.makeId(10) + new Date().getTime() + this.element.data('ItemCounter');
             var data = '<li>' + '<a id = "'+ itemId +'">' + item['label'] + '</a>';
+
+            this.element.data('ItemCounter', this.element.data('ItemCounter') + 1);
 
             if(item['subItems'] !== null && !item['active'])
             {
@@ -166,18 +198,18 @@
 
             if( $.isFunction(item['onclick']) )
             {
-                this.global_vars.OnClickCallbacks[itemId] = item['onclick'];
+                this.element.data('OnClickCallbacks')[itemId] = item['onclick'];
             }
             else if(typeof item['onclick'] === "string")
             {
-                this.global_vars.OnClickCallbacks[itemId] = function (){ window.location.href = item['onclick'];};
+                this.element.data('OnClickCallbacks')[itemId] = function (){ window.location.href = item['onclick'];};
             }
             else
             {
-                this.global_vars.OnClickCallbacks[itemId] = function (){ return false;};
+                this.element.data('OnClickCallbacks')[itemId] = function (){ return false;};
             }
 
-            this.global_vars.ItemIds.push(itemId);
+            this.element.data('ItemIds').push(itemId);
 
             if(item['subItems'] !== null)
             {
@@ -188,7 +220,6 @@
 
             return data;
       },
-
       makeId: function(num)
       {
           var text = "";
@@ -204,6 +235,6 @@
 
     });
 
-    
+   
 
     
